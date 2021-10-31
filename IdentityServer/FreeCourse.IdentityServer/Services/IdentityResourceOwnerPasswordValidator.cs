627b45252faf28fa9d739a5de2073d3613c8ee51
Oğuzhan Seleker <seleker17@itu.edu.tr>
2021-11-01 00:30:40 +0300

    backup

---
+++ IdentityServer/FreeCourse.IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -0,0 +1,29 @@
+﻿using FreeCourse.IdentityServer.Models;
+using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.IdentityServer.Services
+{
+    public class IdentityResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public IdentityResourceOwnerPasswordValidator(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var existUser = await _userManager.FindByEmailAsync(context.UserName);
+            if (existUser == null)
+            {
+                var 
+            }
+        }
+    }
+}
