0e892ba19a224b2deff2ab11c50f796294fb24ef
Oğuzhan Seleker <seleker17@itu.edu.tr>
2021-11-04 02:04:30 +0300

    BasketApi added

---
+++ Services/Basket/FreeCourse.Services.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        public int Quantity { get; set; }
+        public string CourseId { get; set; }
+        public string courseName { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
