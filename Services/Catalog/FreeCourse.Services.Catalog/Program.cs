bc67ae70d804524a7850d1add1c47b084d21530c
Oğuzhan Seleker <seleker17@itu.edu.tr>
2021-11-16 01:46:35 +0300

    Gateway Api added

--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,5 +1,7 @@
+using FreeCourse.Services.Catalog.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +15,20 @@ namespace FreeCourse.Services.Catalog
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var categoryServices = serviceProvider.GetRequiredService<ICategoryService>();
+                if (!categoryServices.GetAllAsync().Result.Data.Any())
+                {
+                    categoryServices.CreateAsync(new Dtos.CategoryCreateDto { Name = "Asp.net Core Kursu1" }).Wait();
+                    categoryServices.CreateAsync(new Dtos.CategoryCreateDto { Name = "Asp.net Core Kursu2" }).Wait();
+                    categoryServices.CreateAsync(new Dtos.CategoryCreateDto { Name = "Asp.net Core Kursu3" }).Wait();
+                }
+                
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
